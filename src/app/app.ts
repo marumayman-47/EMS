f0dfe522bc78b2fa4e921f9c4e7cffac327f585f
AmiraMohamed56 <awadet3469@gmail.com>
2025-10-14 14:52:13 +0300

    add localstorage logic and mock data

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LocalStorageService } from './services/local-storage';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,15 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('ems');
+// export class App {
+//   protected readonly title = signal('ems');
+// }
+export class App implements OnInit {
+    protected readonly title = signal('ems');
+
+  constructor(private localStorageService: LocalStorageService) {}
+
+  ngOnInit(): void {
+    this.localStorageService.initializeData();
+  }
 }
