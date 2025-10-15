dd55ed8614c91b615cee2b727fa3392b783006fd
AmiraMohamed56 <awadet3469@gmail.com>
2025-10-15 18:43:24 +0300

    update guest routes and interface

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { Login } from './components/login/login';
 import { Register } from './components/register/register';
 import { Home } from './dashboard/home/home';
 import { authGuard } from './services/auth-guard';
+import { guestRoutes } from './guest/guest-routes';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full'  },
     { path: 'login', component: Login }, 
     { path: 'register', component: Register }, 
     { path: 'dashboard', component: Home, canActivate: [authGuard] }, 
-    { path: 'events', component: Eventlist }
+    { path: 'events', component: Eventlist },
+    { path: 'guest', children: guestRoutes}
 ];
