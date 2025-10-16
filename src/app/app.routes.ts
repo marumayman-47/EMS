9add6be3eb4f8be75678e92be59ddd5a5833552a
syntax-weaver <starb3936@gmail.com>
2025-10-16 17:41:25 +0300

    update side and nav bars

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,12 @@ import { authGuard } from './services/auth-guard';
 import { guestRoutes } from './guest/guest-routes';
 import { EventManage } from './dashboard/events/event-manage/event-manage';
 import { Layout } from './dashboard/layout/layout';
+import { EventView } from './dashboard/events/event-view/event-view';
+import { Guests } from './dashboard/guests/guests';
+import { Tasks } from './dashboard/tasks/tasks';
+import { Expenses } from './dashboard/expenses/expenses';
+import { Feedbacks } from './dashboard/feedbacks/feedbacks';
+import { Settings } from './dashboard/settings/settings';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full'  },
@@ -20,12 +26,20 @@ export const routes: Routes = [
         children: [
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: Home },
+            { path: 'events', component: EventView },
+            { path: 'guests', component: Guests },
+            { path: 'tasks', component: Tasks },
+            { path: 'expenses', component: Expenses },
+            { path: 'feedback', component: Feedbacks },
+            { path: 'settings', component: Settings },
         ]
      }, 
     // ============ end organizer dashboard routes ====================
     
     { path: 'events', component: Eventlist },
     { path: 'guest', children: guestRoutes},
-    { path: 'dashboard/events/event-manage', component: EventManage }
+    { path: 'dashboard/events/event-manage', component: EventManage },
 
+    // =================================================================
+    { path: '**', redirectTo: 'login' }
 ];
